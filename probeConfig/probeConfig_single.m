62178982c509c4ff785a49e2d675d8849f203119
Kristina <nielsenlabmbi@gmail.com>
2022-09-01 17:43:42 -0400

    generic probe config file

---
+++ probeConfig/probeConfig_single.m
@@ -0,0 +1,10 @@
+%generate wiring information for single electrode
+%standard format for columns in probewiring:
+%column 1: channel number
+%column 2: x
+%column 3: y
+%column 4: z
+%column 5: shaft
+function probewiring=probeConfig_single
+
+probewiring=[1 0 0 0 1];
