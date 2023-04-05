52bf99d024a89885e49afc4a2c84441c200e4077
Kristina <nielsenlabmbi@gmail.com>
2023-04-05 17:00:54 -0400

    update to MU processing

--- MU analysis/mergeMUspkInfo.m
+++ MU analysis/mergeMUspkInfo.m
@@ -1,9 +1,11 @@
 function mergeMUspkInfo(filepath,animalID,unitID,expID,probeID,startID,stopID)
 
 %this function merges all MU spike files into one file for further analysis
+%will remove events flagged as duplicates to stay consistent with the
+%spkSort file
 %
 %input:
-%filepath: path to the animal folder (e.g.., Z:\EphysNew\Data)
+%filepath: path to the animal folder (e.g., Z:\EphysNew\Data)
 % animalID - animal ID (string)
 % unitID - unit ID (string)
 % expID - experiment ID (string)
@@ -17,10 +19,6 @@ function mergeMUspkInfo(filepath,animalID,unitID,expID,probeID,startID,stopID)
 %detCh: detection channel for each spike
 %detChSort: detection channels reorganized according to z position
 %(bottom of probe is channel 1 in this organization)
-%flagDuplicate: 1 - spike is duplicate of spike on other channel
-%duplicateMxCh: channel of the spike considered the original
-%duplicateMxIdx: event index (job file specific) of the spike considered
-%the original
 %NDuplicate: number of cooccuring spike along the entire probe
 %jobId: job file a spike is stored in
 
@@ -37,37 +35,19 @@ for i=startID:stopID
     
     %output
     if i==startID
-        MUspkMerge.spktimes=spk.spkTimesDet;
-        MUspkMerge.detCh=spk.detCh;
-        MUspkMerge.detChSort=spk.detChSort;
-        MUspkMerge.flagDuplicate=spk.flagDuplicate;
-        MUspkMerge.NDuplicate=spk.NDuplicate;
-        MUspkMerge.jobId=ones(size(spk.spkTimesDet))*i;
-        MUspkMerge.duplicateMxCh=spk.duplicateMxCh;
-        MUspkMerge.duplicateMxIdx=spk.duplicateMxIdx;
+        idx=find(spk.flagDuplicate==0);
+        MUspkMerge.spktimes=spk.spkTimesDet(idx);
+        MUspkMerge.detCh=spk.detCh(idx);
+        MUspkMerge.detChSort=spk.detChSort(idx);
+        MUspkMerge.NDuplicate=spk.NDuplicate (idx);
+        MUspkMerge.jobId=ones(size(idx))*i;
     else
-        MUspkMerge.spktimes=[MUspkMerge.spktimes spk.spkTimesDet];
-        MUspkMerge.detCh=[MUspkMerge.detCh spk.detCh];
-        MUspkMerge.detChSort=[MUspkMerge.detChSort spk.detChSort];
-        MUspkMerge.flagDuplicate=[MUspkMerge.flagDuplicate spk.flagDuplicate];
-        MUspkMerge.NDuplicate=[MUspkMerge.NDuplicate spk.NDuplicate];
-        MUspkMerge.jobId=[MUspkMerge.jobId ones(size(spk.spkTimesDet))*i];
-        
-        %merging of duplicate channels and idx requires expansion of
-        %matrices to make things fit
-        nRowIn=size(spk.duplicateMxCh,1);
-        nRowOut=size(MUspkMerge.duplicateMxCh,1);
-        if nRowIn>nRowOut
-            MUspkMerge.duplicateMxCh(nRowIn,:)=0;
-            MUspkMerge.duplicateMxIdx(nRowIn,:)=0;
-        end
-        if nRowIn<nRowOut
-            spk.duplicateMxCh(nRowOut,:)=0;
-            spk.duplicateMxIdx(nRowOut,:)=0;
-        end
-        MUspkMerge.duplicateMxCh=[MUspkMerge.duplicateMxCh spk.duplicateMxCh];
-        MUspkMerge.duplicateMxIdx=[MUspkMerge.duplicateMxIdx spk.duplicateMxIdx];
-        
+        idx=find(spk.flagDuplicate==0);
+        MUspkMerge.spktimes=[MUspkMerge.spktimes spk.spkTimesDet(idx)];
+        MUspkMerge.detCh=[MUspkMerge.detCh spk.detCh(idx)];
+        MUspkMerge.detChSort=[MUspkMerge.detChSort spk.detChSort(idx)];
+        MUspkMerge.NDuplicate=[MUspkMerge.NDuplicate spk.NDuplicate(idx)];
+        MUspkMerge.jobId=[MUspkMerge.jobId ones(size(idx))*i];
     end
     clear spk;
 end
