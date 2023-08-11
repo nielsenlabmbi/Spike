8c3c7949ef52d3e8c1fe43261e5b60f65b66125f
Kristina <nielsenlabmbi@gmail.com>
2023-08-11 16:28:54 -0400

    merge intan progress

--- util/mergeIntan.m
+++ util/mergeIntan.m
@@ -22,33 +22,76 @@ mergeInfo.files=mergeStruct;
 mergeInfo.name=name;
 mergeInfo.date=date;
 mergeInfo.filesize=zeros(1,length(mergeStruct)); %necessary info for splitting files later
+mergeInfo.bufferLength=1000;
 
-%% merge amplifier files
-%make directory for output
+%% make directory for output
 outname=[animalID '_uMMM_' outID];
-outAmp=fullfile(expFolder,animalID,outname,[outname '_amplifier.dat']);
 if ~exist(fullfile(expFolder,animalID,outname),'dir')
     mkdir(fullfile(expFolder,animalID),outname);
 end
 
 
-%total number of jobs for waitbar
-totJobs=length(mergeStruct)*parts; %pretending the first one is divided into parts too
+%% merge headers by making a minimal header for the new file 
+%only need sampleFreq and stuff before it
+%we could copy one of the headers, but this way it's clear that these are
+%not raw intan files
+%this also provides the number of channels (needed for buffer)
 
-%simply copy the first element of mergeStruct, renaming it in the process
-h=waitbar(parts/totJobs,'Merging files...');
-mergeFile=fullfile(expFolder,animalID,[animalID '_' mergeStruct{1}],[animalID '_' mergeStruct{1} '_amplifier.dat']);
-copyfile(mergeFile,outAmp);
+%read the first header - this only works if the sample freq is the same
+%throughout anyways
+headerFile=fullfile(expFolder,animalID,[animalID '_' mergeStruct{1}],[animalID '_' mergeStruct{1} '_info.rhd']);
+headerIn=read_Intan_Header(headerFile);
+
+%get number of channels
+mergeInfo.nChannel=sum(headerIn.signal_group_num_amp_channels(headerIn.signal_group_enabled==1));
+
+%generate output file
+hOut=fullfile(fullfile(expFolder,animalID,outname,[outname '_info.rhd']));
+fid = fopen(hOut, 'w');
+
+%write magic number
+magic_number= 0xC6912702;
+fwrite(fid,magic_number,'uint32');
+
+%fake file version
+main_version=0;
+second_version=0;
+fwrite(fid,main_version,'int16');
+fwrite(fid,second_version,'int16');
+
+%sample rate
+fwrite(fid,headerIn.sample_rate,'single');
+fclose(fid);
+
+
+%% merge amplifier files
+%the first file could just be copied, but in the end we need to open it for
+%reading anyways - just treat like ther est
+
+%waitbar
+totJobs=length(mergeStruct)*parts; 
+h=waitbar(0/totJobs,'Merging files...');
 
 %open output for write
-outFID = fopen(outAmp,'a'); 
-fseek(outFID,0,'eof'); % move to end of file
-mergeInfo.filesize(1)=ftell(outFID); %number of bytes in first file
+outAmp=fullfile(expFolder,animalID,outname,[outname '_amplifier.dat']);
 
+%check that this does not exist already
+if exist(outAmp,'file')
+    errordlg('Merge file already exists, aborting!','Error');
+    return;
+end
 
-%then go through the rest, read and append; broken into parts to make it
+outFID = fopen(outAmp,'a+'); 
+
+%go through files, read and append; broken into parts to make it
 %manageable
-for f=2:length(mergeStruct)
+for f=1:length(mergeStruct)
+    %we need a buffer to mask the transition between the files (filtering
+    %creates an artefact otherwise)
+    if f>1
+        fwrite(outFID,zeros(1,buffer),'int16');
+    end
+    
     mergeFile=fullfile(expFolder,animalID,[animalID '_' mergeStruct{f}],[animalID '_' mergeStruct{f} '_amplifier.dat']);
     fileinfo = dir(mergeFile);
     mergeInfo.filesize(f)=fileinfo.bytes;
@@ -68,6 +111,8 @@ for f=2:length(mergeStruct)
     data = fread(mergeFID,samplesLeft, 'int16');
     fwrite(outFID,data,'int16');
  
+    %use the data for transition to the next one
+
     fclose(mergeFID);
 end
 
@@ -75,32 +120,6 @@ end
 fclose(outFID);
 close(h);
 
-%% merge headers by making a minimal header for the new file 
-%only need sampleFreq and stuff before it
-%we could copy one of the headers, but this way it's clear that these are
-%not raw intan files
-
-%read the first header - this only works if the sample freq is the same
-%throughout anyways
-headerFile=fullfile(expFolder,animalID,[animalID '_' mergeStruct{1}],[animalID '_' mergeStruct{1} '_info.rhd']);
-headerIn=read_Intan_Header(headerFile);
-
-hOut=fullfile(fullfile(expFolder,animalID,outname,[outname '_info.rhd']));
-fid = fopen(hOut, 'w');
-
-%write magic number
-magic_number= 0xC6912702;
-fwrite(fid,magic_number,'uint32');
-
-%fake file version
-main_version=0;
-second_version=0;
-fwrite(fid,main_version,'int16');
-fwrite(fid,second_version,'int16');
-
-%sample rate
-fwrite(fid,headerIn.sample_rate,'single');
-fclose(fid);
 
 
 %% save merge info file
